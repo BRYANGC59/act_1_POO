02eacb66ead2a4b2404796f35671dfc766203233
ValeriaMonsalve <valeriaalvarez448@gmail.com>
2025-02-18 13:26:50 -0500

    Creation parameters

--- app/notebook.py
+++ app/notebook.py
@@ -6,6 +6,14 @@ class Note:
     MEDIUM: str = "MEDIUM"
     LOW: str = "LOW"
 
+    def __int__(self, code: str, title: str, text: str, importance: str):
+        self.code: str = code
+        self.title: str = title
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date = datetime.now()
+        self.tags: list[str] = []
+
 
 
 
